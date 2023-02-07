90c21daaf1c5678f52e3872f7e379b0ffd7cc75a
zinb9419 <zinb9419@gmail.com>
2023-02-07 15:59:49 +0100

    c03

---
+++ ex00/ClapTrap.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ziloughm <ziloughm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/07 12:28:20 by ziloughm          #+#    #+#             */
+/*   Updated: 2023/02/07 15:50:06 by ziloughm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#   ifndef CLAPTRAP_H
+# define CLAPTRAP_H
+
+#include <iostream>
+#include <string>
+#include "ClapTrap.hpp"
+
+#define GRN "\033[0;32m"
+#define WHT "\033[0;0m"
+#define RED "\033[0;31m"
+
+#   endif
---
+++ ex00/main.cpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ziloughm <ziloughm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/07 12:28:03 by ziloughm          #+#    #+#             */
+/*   Updated: 2023/02/07 15:58:09 by ziloughm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.h"
+
+int main(void)
+{
+    ClapTrap a;
+    ClapTrap b("test2");
+    ClapTrap c("test1");
+    ClapTrap d(b);
+
+    a = c;
+
+    std::cout << RED << "/----------------------------/" << WHT << std::endl;
+    std::cout << GRN << "---------   Test1   ---------" << WHT << std::endl;
+    a.attack("objet 1");
+    a.beRepaired(10);
+    a.attack("objet 2");
+    a.beRepaired(10);
+    a.attack("objet 3");
+    a.beRepaired(10);
+    a.attack("objet 4");
+    a.beRepaired(100);
+    a.attack("objet 5");
+    a.beRepaired(100);
+    a.attack("objet 6");
+    std::cout << a;
+    std::cout << GRN << "--------- End Test1 ---------" << WHT << std::endl;
+    std::cout << RED << "/----------------------------/" << WHT << std::endl << std::endl;
+    
+    std::cout << RED << "/----------------------------/" << WHT << std::endl;
+    std::cout << GRN << "---------   Test2   ---------" << WHT << std::endl;
+    b.takeDamage(8);
+    b.attack("objet 5");
+    b.takeDamage(1);
+    b.beRepaired(100);
+    b.takeDamage(10);
+    std::cout << b;
+    std::cout << GRN << "--------- End Test2 ---------" << WHT << std::endl;
+    std::cout << RED << "/----------------------------/" << WHT << std::endl << std::endl;
+
+    return(0);
+}
